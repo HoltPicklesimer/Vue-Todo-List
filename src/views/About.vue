dfbe3727c71f3d1c52a27641d0e71a61312a8cdb
Holt Picklesimer <holtpicklesimer@gmail.com>
2020-10-25 18:33:57 -0600

    Created Todo List app.

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is a TodoList app v1.0.0. It was created by following the Vue crash course by Traversy Media on Youtube: https://www.youtube.com/watch?v=Wy9q22isx3U&t=1170s.</p>
+  </div>
+</template>
